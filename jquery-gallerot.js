b3bbd7cde75bac32e82e9f63fbc63d71eeb57ac6
Vadim Zverugo <vadim.zverugo@gmail.com>
2012-11-01 19:32:50 +0200

    Refactoring

--- jquery-gallerot.js
+++ jquery-gallerot.js
@@ -22,12 +22,12 @@
         leftSlideIndex = 0;
 
         // Positioning and sizing
-        var baseContainerWidth = params.width != null ? params.width : $(baseContainer).parent().innerWidth();
-        var baseContainerHeight = params.height != null ? params.height : $(baseContainer).parent().innerHeight();
+        var baseContainerWidth = params.width != null ? params.width : $(baseContainer).parent().width();
+        var baseContainerHeight = params.height != null ? params.height : $(baseContainer).parent().height();
         $(baseContainer).css({
             position: 'relative',
-            width: (baseContainerWidth + 'px'),
-            height: (baseContainerHeight + 'px'),
+            width: baseContainerWidth,
+            height: baseContainerHeight,
             margin: 0,
             padding: 0,
             border: 'none',
@@ -46,13 +46,11 @@
         });
         $(slidesContainer).css({
             position: 'relative',
-            width: (slidesOverallWidth + 'px'),
+            width: slidesOverallWidth,
             listStyle: 'none',
             margin: 0,
             padding: 0,
-            border: 'none',
-            left: 0,
-            top: 0
+            border: 'none'
         });
 
         // Action listeners
@@ -60,7 +58,7 @@
         $(params.rightControl).bind('click', slideRight);
 
         if (params.enableAutoSliding) {
-            // TODO: Enable auto sliding using Timer.
+            // TODO: Enable auto sliding using jQuery.delay()
         }
         return this;
     };
@@ -68,42 +66,40 @@
     var slideLeft = function() {
         if (leftSlideIndex > 0) {
             leftSlideIndex -= 1;
-            var slidersContainerLeft = 0;
-            $(slides).each(function(i, slide) {
-                if (i < leftSlideIndex) {
-                    slidersContainerLeft += $(slide).width();
-                }
-            });
-            moveSlidersContainer(slidersContainerLeft);
+            moveSlidesContainerTo(leftSlideIndex);
         }
     };
 
     var slideRight = function() {
         if (leftSlideIndex < (slides.length - 1)) {
             leftSlideIndex += 1;
-            var slidersContainerLeft = 0;
-            $(slides).each(function(i, listItem) {
-                if (i < leftSlideIndex) {
-                    slidersContainerLeft += $(listItem).width();
-                }
-            });
-            moveSlidersContainer(slidersContainerLeft);
+            moveSlidesContainerTo(leftSlideIndex);
         } else if (leftSlideIndex == (slides.length - 1)) {
-            /*var l = $(slidesContainer).position().left;
-            var l2 = l - 100;
-            $(slidesContainer).animate({
-                left: (l2 + 'px')
-            }, params.slidingSpeed);
-            $(slidesContainer).animate({
-                left: ('0px')
-            }, (params.slidingSpeed / slides.length));
-            leftSlideIndex = 0;*/
+            var slidesContainerLeft = $(slidesContainer).position().left;
+            var slidesContainerShiftLeft = slidesContainerLeft - ($(slides).last().width() / 2);
+            moveSlidesContainerOn(slidesContainerShiftLeft, (params.slidingSpeed / 2));
+            leftSlideIndex = 0;
+            moveSlidesContainerTo(leftSlideIndex);
         }
     };
 
-    var moveSlidersContainer = function(left) {
-        $(slidesContainer).animate({
-            left: ('-' + left + 'px')
-        }, params.slidingSpeed);
+    var moveSlidesContainerOn = function(leftPos, speed) {
+        if (speed === undefined) {
+            speed = params.slidingSpeed;
+        }
+        $(slidesContainer).animate({left: leftPos}, speed);
+    };
+
+    var moveSlidesContainerTo = function(slideIndex, speed) {
+        if (speed === undefined) {
+            speed = params.slidingSpeed;
+        }
+        var slidersContainerLeft = 0;
+        $(slides).each(function(i, slide) {
+            if (i < slideIndex) {
+                slidersContainerLeft += $(slide).width();
+            }
+        });
+        $(slidesContainer).animate({left: -slidersContainerLeft}, speed);
     };
 })(jQuery);
